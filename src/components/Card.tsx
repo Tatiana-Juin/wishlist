903f077ec3cfaca70d8b91627235cc710fcd9c1e
Tatiana-Juin <username@users.noreply.github.com>
2026-04-05 18:43:38 +0200

    J'ai commencer à faire les modification par rapport au distance des liens

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,12 +9,18 @@ export default function Card({product}:{product:Products}) {
       
       
       {/* Pour lesliens est modification */}
-      <div className="border border-pink-200 bg-pink-100 rounded-lg">
-          <a href={product.url} target="__blanck" className="mr-10  mt-5   bg-pink-100 p-2 "><i className="bi bi-arrow-up-right"></i></a>
-          {/* modification */}
-          <button className=" mt-5  bg-pink-100 p-2 cursor-pointer mr-10"><i className="bi bi-pencil-fill"></i></button>
-          {/* Supprimer */}
-          <button className=" mt-5   bg-pink-100 p-2 cursor-pointer"><i className="bi bi-trash"></i></button>
+      <div className="border border-pink-200 bg-pink-100 rounded-lg flex items-center justify-between ">
+        
+        <a href={product.url} target="__blanck" className="mr-10  mt-2  bg-pink-100 p-2 "><i className="bi bi-arrow-up-right"></i></a>
+    
+         
+        <div className="mt-2 flex gap-2">
+            {/* modification */}    
+          <button className="  bg-pink-100  cursor-pointer mr-10"><i className="bi bi-pencil-fill"></i></button>
+        {/* Supprimer */}
+          <button className="  bg-pink-100 p-2 cursor-pointer"><i className="bi bi-trash"></i></button>
+        </div>
+          
       </div>
       
       
