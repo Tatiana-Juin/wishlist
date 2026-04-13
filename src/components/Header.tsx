49553387391d8bbaba599243f4640201e687080b
Tatiana-Juin <username@users.noreply.github.com>
2026-04-13 11:38:07 +0200

    Il me reste plus qu'a faire le total

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,11 @@
 export default function Header() {
   return (
     <>
-      <header>
+      <header >
           <h1 className="text-3xl font-bold underline text-center mt-6">
               wishlist - liste des souhait 
           </h1>
+          <h2 className="bold">Total</h2>
       </header>
     </>
   )
